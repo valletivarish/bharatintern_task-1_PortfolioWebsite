d3c00d2b8db2a3df3abfbf52d80719c97643725e
Valleti Varish <150535998+valletivarish@users.noreply.github.com>
2024-02-28 21:59:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+var typed = new Typed("#element", {
+  strings: ["Developer", "Designer"],
+  typeSpeed: 150,
+  backspeed: 150,
+  loop: true,
+});
+// let numberPercent = document.querySelectorAll(".countbar");
+// let getPercent = Array.from(numberPercent);
+
+// getPercent.map((items) => {
+//   let startCount = 0;
+//   let progressBar = () => {
+//     startCount++;
+//     items.innerHTML = `<h3>${startCount}%</h3>`;
+//     items.style.width = `${startCount}%`;
+//     if (startCount == items.dataset.percentnumber) {
+//       clearInterval(stop);
+//     }
+//   };
+//   let stop = setInterval(() => {
+//     progressBar();
+//   }, 25);
+// });
